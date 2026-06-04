b4b813979f1b278376e57dfc6d9124e597babd40
polina-bobko <its.pdv@gmail.com>
2026-06-04 22:29:23 +0200

    Minor changes

--- logger.py
+++ logger.py
@@ -13,13 +13,13 @@ logger = logging.getLogger(__name__)
 def logger_decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        logger.info(f'Вызов функции: {func.__name__}')
+        logger.info(f'Call the function: {func.__name__}')
         try:
             result = func(*args, **kwargs)
-            logger.info(f'Функция {func.__name__} выполнена успешно')
+            logger.info(f'Function {func.__name__} successfully called.')
             return result
         except Exception as e:
-            logger.error(f'Ошибка в функции {func.__name__}: {e}')
+            logger.error(f'Error in function {func.__name__}: {e}')
             raise e
 
     return wrapper
